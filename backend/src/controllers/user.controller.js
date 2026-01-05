bbb3e9ee10b2de2ab8dfc28bd7b62bfd556d9926
MurtazaMirzad01 <murtazamirzad01@gmail.com>
2026-01-05 18:10:59 +0430

    feat: Create logout API

--- backend/src/controllers/user.controller.js
+++ backend/src/controllers/user.controller.js
@@ -68,8 +68,32 @@ const loginUser = async(req, res) => {
     }
 }
 
+const logoutUser = async (req, res) =>{
+    try{
+        const {email} = req.body;
+
+        const user = User.findOne({
+            email
+        })
+        if (!user) return res.status(400).json({
+            message: "user not found!"
+        });
+
+        res.status(200).json({
+            message: "Loged out successfully"
+        });
+
+    }catch(error){
+        res.status(500).json({
+            message: "Internal Server Error",
+            error: error.message
+        });
+    }
+}
+
 
 export {
     registerUser,
-    loginUser
+    loginUser,
+    logoutUser
 }
